db61d8cbf4c38b210715290ce40c9b4b78e45dd3
Per Unneberg <per.unneberg@scilifelab.se>
2021-10-11 22:38:37 +0200

    Test hidden environments file

--- condaenv.py
+++ condaenv.py
@@ -5,15 +5,40 @@ import re
 import sys
 import hashlib
 import urllib
+import argparse
 from collections import defaultdict
 from snakemake.workflow import Workflow
 from snakemake.dag import DAG
 
-rootdir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-snakefile = os.path.join(os.path.join(rootdir, "Snakefile"))
-# wrapper_prefix = os.path.join(rootdir, "workflow", "wrappers")
+parser = argparse.ArgumentParser(
+    """
+Parse snakemake workflow and retrieve conda environment files.
+"""
+)
+parser.add_argument(
+    "--wrapper-prefix",
+    metavar="wrapper_prefix",
+    type=str,
+    help="wrapper prefix",
+    default=None
+)
+parser.add_argument(
+    "--snakefile",
+    metavar="snakefile",
+    type=str,
+    help="snakefile",
+    default="Snakefile"
+)
 
-wf = Workflow(snakefile=snakefile) # , wrapper_prefix=f"file:{wrapper_prefix}")
+args = parser.parse_args()
+wrapper_prefix = args.wrapper_prefix
+snakefile = args.snakefile
+
+if wrapper_prefix is not None:
+    wrapper_prefix=f"file:{wrapper_prefix}"
+rootdir = os.path.abspath(os.path.dirname(snakefile))
+
+wf = Workflow(snakefile=snakefile, wrapper_prefix=wrapper_prefix)
 wf.include(snakefile, overwrite_first_rule=True)
 wf.check()
 
@@ -29,11 +54,11 @@ for rule in wf.rules:
         contents = "".join(response.read().decode())
     else:
         with open(fn, "r") as fh:
-            contents = "".join(fh.readlines())
+            contents = "".join(fh.readlines()).strip()
     md5hash.update(contents.encode())
     envs[md5hash.hexdigest()] = contents
 
 md5hash = hashlib.md5()
 for k, v in envs.items():
     md5hash.update(v.encode())
-print(md5hash.hexdigest())
+    print(v)
